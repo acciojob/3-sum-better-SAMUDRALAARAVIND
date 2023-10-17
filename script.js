21e1087d19be7c2f8a6fbe76cd7a58eb8869dbb0
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2023-10-17 14:27:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function threeSum(arr, target) {
 	let gap = Infinity, ans = -1; 
 	for(let i = 0; i < arr.length- 2; i++){
 		for(let j = i+1; j < arr.length - 1; j++){
-			for(let k = j + 1; j < arr.length ; k++){
+			for(let k = j + 1; k < arr.length ; k++){
 				let currentSum = arr[i] + arr[j] + arr[k] ;
 				let diff = Math.abs(currentSum - target);
 				if(diff < gap){
